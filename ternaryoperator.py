e0d0101cba70286bbc8c9dc77d0199753a25a26f
Akhil Cherian Jacob <84498957+akhilcj@users.noreply.github.com>
2024-08-06 13:51:42 +0530

    Add files via upload

---
+++ ternaryoperator.py
@@ -0,0 +1,12 @@
+age = 16
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Eligible"
+
+
+print(message)
+
+age = 18
+message = "Eligible" if age>=18 else "Not Eligible"
+print(message)
